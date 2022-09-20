934c4ae9bc5422b079012d1998ab5ce99c3d0d1b
Your Name <amohm001@plattsburgh.edu>
2022-09-20 10:39:44 +0300

    working on search

--- lib/main.dart
+++ lib/main.dart
@@ -2,44 +2,140 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() {
+  String? word = listSample[0]["amir"];
+  print(word);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String query = '';
+  List<String> searchResults= [];
+  @override
   Widget build(BuildContext context) {
-    // final wordPair = WordPair.random();
+    const title = 'Basic List';
+    final List<Map<String, String>> listSample = [
+      {
+        "ditamo": "male, example: he's name is ditamo",
+        "mekya": "female, Example: Her name is mekya" ,
+        "amir": "amir: \n meaning: male \n Example: He's gender is male",
+        "eman": "female"
+      }
+    ];
+    //
+    void initState() {
+      super.initState();
+
+
+    }
+
+    _search(String text) {
+      searchResults.clear();
+      listSample[0].forEach((key, value) {
+        if (key.contains(text) || value.contains(text))
+          searchResults.add(value);
+      });
+    }
+    TextEditingController _controller = TextEditingController();
+    String? word = listSample[0]["amir"];
+
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
+        title: "Dictionary",
+        home: Scaffold(
+          appBar: AppBar(
+            title: Center(child: Text("Dictionary")),
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(50.0),
+              child:
+                Container(
+                    width: double.infinity,
+                    height: 40,
+                    color: Colors.white,
+                    child: TextField(
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintText: 'Search for something',
+                        prefixIcon: Icon(Icons.search),
+                      ),
+                      onChanged:  _search,
+
+                    )
+
+
+                )
+            //     new Expanded(
+            // child: searchResults.length!=  0 || _controller.text.isNotEmpty ?
+            // new ListView.builder(
+            //     itemCount: searchResults.length,
+            //   itemBuilder: (context, i){
+            //
+            //   }
+            // )))
+
+                ),
+                  // IconButton(
+                  //     icon: Icon(
+                  //         Icons.search,
+                  //         color: Colors.red
+                  //     ), onPressed: () {  },
+                  // )
+
+            ),
+
+
+          body: Container(
+              child: Center(
+                  child: Text(
+                    listSample[0].keys.toString(),
+                    textAlign: TextAlign.center,)
+
+              )
+
+          )
+
+
+            // child: Text(listSample[0]["amir"]!, textAlign: TextAlign.center,)
+
+
+
+        // appBar: AppBar(
+        //   title: const Text(title),
+        // ),
+        // body: ListView(
+        //   children: <Widget>[
+        //     Container(
+        //     color: Colors.white,
+        //     child: Center(child: Text(listSample[0]["amir"]!, textAlign: TextAlign.center,)),
+        //     ),
+        //
+        //   ],
+         ),
     );
+
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+final List<Map<String, String>> listSample = [
+  {
+    "amir": "meaning: male \n Example: He's gender is male",
+    "eman": "female"
   }
-}
+  ];
+
+
